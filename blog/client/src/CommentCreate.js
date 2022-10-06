9577dac4f71b432081cd17b6cb470c88b819ac7b
anuj66 <saraogi.anuj9@gmail.com>
2022-10-06 23:32:25 +0530

    (Blog) Added Comment List

--- blog/client/src/CommentCreate.js
+++ blog/client/src/CommentCreate.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const CommentCreate = ({ postId }) => {
-  return <div>CommentCreate</div>;
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(
+      `http://localhost:4001/posts/${postId}/comments`,
+      {
+        content,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label className="my-2">New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control my-2"
+          />
+        </div>
+        <button className="btn btn-primary my-2">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default CommentCreate;
