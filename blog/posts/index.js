c791735032d43a4867f6f94d30fcd32a305ea3a7
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2023-01-08 23:02:51 +0530

    Added Posts and Event Bus to Kubernetes

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -23,7 +23,7 @@ app.post("/posts", async (req, res) => {
 
     console.log("Posts : ", posts);
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       event: {
         type: "PostCreated",
         data: {
